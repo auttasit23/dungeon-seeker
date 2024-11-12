7a15ae25a6cbb66aa927559471afae2e687d569b
auttasit23 <auttasit.umpo@bumail.net>
2024-11-12 11:48:44 +0700

    Fix Potion Error

--- Assets/Scripts/OOPItemPotion.cs
+++ Assets/Scripts/OOPItemPotion.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,31 @@ namespace Searching
 
     public class OOPItemPotion : Identity
     {
-        
+        private void Update()
+        {
+            if (mapGenerator.potion.ContainsKey(transform.position))
+            {
+                mapGenerator.potion[transform.position].Remove(this);
+                if (mapGenerator.potion[transform.position].Count == 0)
+                {
+                    mapGenerator.potion.Remove(transform.position);
+                }
+            }
+            
+            if (!mapGenerator.potion.ContainsKey(transform.position))
+            {
+                mapGenerator.potion[transform.position] = new List<OOPItemPotion>();
+            }
+            mapGenerator.potion[transform.position].Add(this);
+        }
 
         public override void Hit()
         {
-            mapGenerator.player.Heal(25);
-            Destroy(gameObject);
+            if (this != null)
+            {
+                mapGenerator.player.Heal(25);
+                Destroy(gameObject);
+            }
         }
     }
 }
